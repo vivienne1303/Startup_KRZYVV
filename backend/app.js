6f15be090d21667c47f3b15509cd0cd5cd2b751c
vivienne1303 <viviennelau007@gmail.com>
2026-07-03 10:50:37 +0800

    Add Netlify identity widget

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const path = require("path");
 const apiRoutes = require("./routes");
 const errorHandler = require("./middleware/errorHandler");
 const notFoundHandler = require("./middleware/notFoundHandler");
@@ -14,6 +15,7 @@ app.use(express.json({ limit: "1mb" }));
 app.use(express.urlencoded({ extended: true, limit: "1mb" }));
 
 app.use("/api", apiRoutes);
+app.use(express.static(path.join(__dirname, "..")));
 
 app.use(notFoundHandler);
 app.use(errorHandler);
